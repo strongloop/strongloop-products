432219456bc72c547ce63ba9822ffe8d4ffe41ec
Raymond Feng <raymond@strongloop.com>
2014-07-18 11:20:57 -0700

    Enhance the post install message

---
+++ strongloop/check-installation.cmd
@@ -0,0 +1,6 @@
+echo The following StrongLoop modules are installed:
+slc version
+echo
+echo generator-loopback version must be \>= 1.0.0.
+echo Please run 'slc update' to make sure all dependencies are up to date.
+echo
